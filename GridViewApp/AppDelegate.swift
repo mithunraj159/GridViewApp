12bac1a482fb26362733866c4c00ac01e471b4d6
mithunraj <mithun.raj159@gmail.com>
2020-08-27 10:01:48 +0530

    Initial Commit

---
+++ GridViewApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GridViewApp
+//
+//  Created by Mithun Raj on 27/08/20.
+//  Copyright © 2020 Mithun Raj. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
